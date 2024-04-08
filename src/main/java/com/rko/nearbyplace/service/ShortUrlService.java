4a3f2592642eca3136f3466037311d03f79fc1c6
Lee Jae Hun <wogns970@naver.com>
2024-04-08 11:58:06 +0900

    [chore] BASE_URL을 환경 변수로 변경
    * jar파일을 하나로 만드는 작업도 같이 진행

--- src/main/java/com/rko/nearbyplace/service/ShortUrlService.java
+++ src/main/java/com/rko/nearbyplace/service/ShortUrlService.java
@@ -2,6 +2,7 @@ package com.rko.nearbyplace.service;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -13,7 +14,8 @@ public class ShortUrlService {
         .maximumSize(10000)
         .expireAfterWrite(24, TimeUnit.HOURS)
         .build();
-    private final String BASE_URL = "http://localhost:8080/";
+    @Value("${BASE_URL}")
+    private String BASE_URL;
 
     public String shortenUrl(String originalUrl) {
         String shortKey = generateRandomKey(8);  // 8자리 랜덤 키 생성
